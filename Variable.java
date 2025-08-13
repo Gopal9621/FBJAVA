6c4d3c3de9a41443adf2bc853d1e159009ed18ed
Gopal Verma <iasgopalverma9621@gmail.com>
2025-08-13 10:00:11 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+   public static int a= 5; 
+   public static void main(String[] arg){
+    int b = 6;// local variable
+    System.out.println("static variable is = " + a);
+    System.out.println("local variable variable is = " + b);
+   }
+}
