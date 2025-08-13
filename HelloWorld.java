645d5575d70f3e5ab89897b0d4d6c8669a59bc3f
Gopal Verma <iasgopalverma9621@gmail.com>
2025-08-13 14:20:27 +0530

    operators in Java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" NAME-- Gopal Verma");
+        System.out.println("class -- OOPs");
+        System.out.println("Roll No. -- 2415000611");
+    }
+}/* */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" HELLO WORLD");
+    }
+}        
