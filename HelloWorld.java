178f18d06c3126c8f4a6c485eea246140500b9b6
Gopal Verma <iasgopalverma9621@gmail.com>
2025-08-13 09:24:21 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" NAME-- Gopal Verma");
+        System.out.println("class -- OOPS");
+        System.out.println("Roll No. -- 2415000611");
+    }
+}
