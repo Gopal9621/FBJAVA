513fc1e5a54d38fc504603b59062119eb7d93b2e
Gopal Verma <iasgopalverma9621@gmail.com>
2025-08-14 10:13:14 +0530

    Add files via upload

---
+++ increment.java
@@ -0,0 +1,15 @@
+// public class increment{
+// public static void main(String[] args){
+//     int a = 5;
+//     ++a;
+//     System.out.println("a = " + a);
+//     a = 10;
+//     --a;
+//     System.out.println("a = " + a);
+//     int b = 10;
+//     b++;
+//     System.out.println("b = " + b);
+// }
+// }
+
+
