0746a9c262c345154093108223b646d04981d038
Gopal Verma <iasgopalverma9621@gmail.com>
2025-08-20 19:30:43 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,39 @@
+public class palindrome{
+    public static void main(String[] args){
+        int num = 121;
+        int original = num;
+        int reverse = 0;
+        
+    while (num != 0){
+        int digit = num%10;
+        reverse = reverse*10 + digit;
+        num  /=  10;
+    }
+    System.out.println("Reverse number is " + reverse);
+
+    if(original == reverse){
+        System.out.println("It is a palindrome");}
+        else{
+            System.out.println("It is not a palindrome");
+        
+    }
+}  
+    }
+
+
+
+// public class palindrome {
+//     public static void main(String[] args) {
+//         int num = 121; // Example number
+//         int reverse = 0;
+
+//         while (num != 0) {
+//             int digit = num % 10;
+//             reverse = reverse * 10 + digit;
+//             num /= 10;
+//         }
+
+//         System.out.println("Reversed number: " + reverse);
+
+//     }    
+//     } 
