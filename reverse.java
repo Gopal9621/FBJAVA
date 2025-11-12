4e69795de54e8d271869b7d85badce3395a7edcc
Gopal Verma <iasgopalverma9621@gmail.com>
2025-11-12 11:25:02 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -8,7 +8,6 @@
 //         System.out.print("Enter a number: ");
 //         int num = sc.nextInt();
 //         int reversed = 0;
-
 //         while(num != 0) {
 //             int digit = num % 10;
 //             reversed = reversed * 10 + digit;
