f5cfa73aeb7df8895662ba0e54e0d98e36049372
Gopal Verma <iasgopalverma9621@gmail.com>
2025-11-15 09:43:47 +0530

    Add files via upload

---
+++ Function/product.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class product {
+    public static int calculateProduct(int a, int b ){
+        // int product = a*b;
+        return a*b;
+    }
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    // int product = calculateProduct(a,b);
+ 
+    System.out.println("product of two number is : "+ calculateProduct(a,b));
+    
+}
+}
