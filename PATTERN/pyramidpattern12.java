0645d6c6e52eca6a511f5093512960fd102acd58
Gopal Verma <iasgopalverma9621@gmail.com>
2025-11-18 23:37:40 +0530

    Enable pyramid pattern printing in pyramidpattern12
    Uncommented the pyramid pattern code to enable execution.

--- PATTERN/pyramidpattern12.java
+++ PATTERN/pyramidpattern12.java
@@ -1,19 +1,19 @@
-// public class pyramidpattern12 {
-//     public static void main(String[] args){
-//         int row = 5;
-//         for(int i = 1; i<= row; i++){
-//             for(int k = 1; k<= row-i; k++){
-//                 System.out.print(" ");
-//             }
-//             for(int j = 1; j <= i; j++){
-//                 System.out.print("* ");
-//             }
-//             System.out.println();
+public class pyramidpattern12 {
+    public static void main(String[] args){
+        int row = 5;
+        for(int i = 1; i<= row; i++){
+            for(int k = 1; k<= row-i; k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
                   
-//         }
+        }
 
-//     }
-// }        
+    }
+}        
 
 //Print Diamond Pattern
 public class pyramidpattern12 {
