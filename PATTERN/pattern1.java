4a0d9f9d996f6da0e6eac871eb539316f159a6d8
Gopal Verma <iasgopalverma9621@gmail.com>
2025-11-21 23:02:41 +0530

    Fix formatting issues in pattern1.java

--- PATTERN/pattern1.java
+++ PATTERN/pattern1.java
@@ -6,7 +6,6 @@ public class pattern1 {
     Scanner sc = new Scanner(System.in);
     int row = sc.nextInt();
     int col = sc.nextInt();
-
     for(int i = 1; i<= row; i++){
         for(int j = 1; j<=col; j++){
             System.out.print(" * ");
