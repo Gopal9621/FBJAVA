659faa6e433f2ff09c5d10ab8f10261787c63ca9
Gopal Verma <iasgopalverma9621@gmail.com>
2025-11-21 23:23:12 +0530

    UPDATE

--- oneDarray.java
+++ oneDarray.java
@@ -1,6 +1,5 @@
 public class oneDarray {
     public static void main(String[] args){
-
         int[] marks = {90, 78, 34, 56, 91};
         for(int i= 0; i< marks.length; i++){
             System.out.println("Element at " + i + " :" +marks[i]);
