f091d31078ec911fcdb410240c0fa01d77701a3a
Gopal Verma <iasgopalverma9621@gmail.com>
2025-11-24 23:50:38 +0530

    Update in CircumCircle.java

--- Function/CircumCircle.java
+++ Function/CircumCircle.java
@@ -3,9 +3,11 @@ public class CircumCircle {
     public static double getCircumference(double r){
          return 2* 3.14 *r;
     }
+    
 public static void main(String[] ags){
     Scanner sc= new Scanner(System.in);
     double r = sc.nextInt();
     System.out.println(" Circumference of circle is : "+getCircumference(r));
 }    
 }
+
