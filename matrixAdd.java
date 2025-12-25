d008127beb892afe3c6646cc24ea57ecbae1d449
Gopal Verma <iasgopalverma9621@gmail.com>
2025-12-25 14:46:00 +0530

    Correct loop counters in matrix addition
    Fix matrix addition logic by adding increment to loop counters.

--- matrixAdd.java
+++ matrixAdd.java
@@ -4,12 +4,10 @@ public class matrixAdd {
         int[][] A = {{1, 2, 3},{4, 5, 6},{2, 3, 4}};
         int[][] B = {{2, 3, 4},{5, 6, 7},{1, 2, 4}};
         int[][] C = new int[3][3];
-
         // System.out.print(A.length);
         for(int i = 0; i < A.length; i++){
           for(int j = 0; j< A[0].length; j++){
             C[i][j] = A[i][j] + B[i][j];
-
           }
         }
         // System.out.println("Matrix Addition");
