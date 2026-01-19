65f03d400310ab4717debcf6fa5a04a9680c5065
Gopal Verma <iasgopalverma9621@gmail.com>
2026-01-19 22:37:59 +0530

    Uncomment increment class and main method

--- increment.java
+++ increment.java
@@ -1,16 +1,16 @@
-// public class increment{
-// public static void main(String[] args){
-//     int a = 5;
-//     ++a;
-//     System.out.println("a = " + a);
-//     a = 10;
-//     --a;
-//     System.out.println("a = " + a);
-//     int b = 10;
-//     b++;
-//     System.out.println("b = " + b);
-// }
-// }
+public class increment{
+public static void main(String[] args){
+    int a = 5;
+    ++a;
+    System.out.println("a = " + a);
+    a = 10;
+    --a;
+    System.out.println("a = " + a);
+    int b = 10;
+    b++;
+    System.out.println("b = " + b);
+}
+}
 
 
 
