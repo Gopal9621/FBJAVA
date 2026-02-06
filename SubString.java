755a347ef78a7a1622aa0d0aaa3809a6a8b49e86
Gopal Verma <iasgopalverma9621@gmail.com>
2026-02-06 23:54:29 +0530

    Update in SubString.java

--- SubString.java
+++ SubString.java
@@ -1,7 +1,8 @@
 public class SubString {
    public static void main(String[] args){
     String sentence = "my name is Gopal Verma";
-    String name = sentence.substring(11 , sentence.length());
+    String name = sentence.substring(11 ,sentence.length());
     System.out.println(name);
 } 
 }
+
