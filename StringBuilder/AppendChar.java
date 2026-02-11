6169420a37b8d5d50aa3db581c8de8ced9e70bf2
Gopal Verma <iasgopalverma9621@gmail.com>
2026-02-11 15:22:01 +0530

    Add files via upload

---
+++ StringBuilder/AppendChar.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AppendChar {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println(sb);
+    }
+}
