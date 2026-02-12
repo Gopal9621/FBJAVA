7734a9a4137741209ff252c0eebdd7d352f42027
Gopal Verma <iasgopalverma9621@gmail.com>
2026-02-13 00:55:12 +0530

    Insert char by StringBuilder

---
+++ StringBuilder/insertChar.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class insertChar {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("tiger ");
+        sb.insert(0,"P");
+        System.out.println(sb);
+    }
+}
+
