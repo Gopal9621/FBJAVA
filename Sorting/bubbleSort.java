111e4e4c52b4352d57c85e650ddaa06806549d33
Gopal Verma <iasgopalverma9621@gmail.com>
2026-02-16 00:00:20 +0530

    Update bubbleSort.java

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -7,7 +7,6 @@ public class bubbleSort {
         for(int i =0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-
         for(int i =0; i<n-1;i++){
             for(int j=0;j<n-i-1; j++){
                 if(arr[j]> arr[j+1]){
@@ -48,3 +47,4 @@ public class bubbleSort {
 //        printArray(arr);
 //    }
 //    }
+
