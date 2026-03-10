e6e28fdbfc3fac6224157324a01fe36561cfa281
Gopal Verma <iasgopalverma9621@gmail.com>
2026-03-10 20:51:41 +0530

    Refactor sumtwoArray class structure

--- Arrays/sumtwoArray.java
+++ Arrays/sumtwoArray.java
@@ -3,10 +3,10 @@ public class sumtwoArray {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {5, 4, 3, 2, 1};
         int[] sum = new int[arr1.length];
-
         for (int i = 0; i < arr1.length; i++) {
             sum[i] = arr1[i] + arr2[i];
         }
+        
         System.out.print("Sum of arrays: ");
         for (int val : sum) {
             System.out.print(val + " ");
@@ -15,5 +15,6 @@ public class sumtwoArray {
 }
 
     
+
 
 
