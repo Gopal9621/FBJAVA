3f42b4e0c3971b77d89c57522b2f4ace2e67538f
Gopal Verma <iasgopalverma9621@gmail.com>
2026-04-08 00:03:52 +0530

    Delete SubString.java

--- SubString.java
+++
@@ -1,8 +0,0 @@
-public class SubString {
-   public static void main(String[] args){
-    String sentence = "my name is Gopal Verma";
-    String name = sentence.substring(11 ,sentence.length());
-    System.out.println(name);
-} 
-}
-
